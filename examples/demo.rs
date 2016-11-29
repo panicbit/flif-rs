e6cb300353a50b5a7f0f773f2fb2a61824b22b6d
panicbit <panicbit.dev@gmail.com>
2016-11-29 17:05:20 +0100

    Enable logging in examples

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,9 +1,11 @@
 extern crate flif;
+extern crate env_logger;
 
 use std::fs::File;
 use std::env;
 
 fn main() {
+    env_logger::init().unwrap();
     let path = env::args().nth(1).expect("usage: demo <image.flif>");
     let mut file = File::open(path).unwrap();
 
